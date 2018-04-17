4badf7a9ee8ae6ad22fd0008291842243a9329ff
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-17 13:49:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount, :title, :price
+
+  def initialize(discount = 0)
+    @array = []
+    @total = total
+    @total = 0
+    @discount = discount
+    @array << @total
+  end
+
+  def add_item(title, price, cost = 1)
+   self.total = cost * price
+   
+
+  end
+
+
+
+
+
+
+end
