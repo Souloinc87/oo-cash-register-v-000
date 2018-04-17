429a58b36328f663d117e9deb4caa832714ea104
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-17 13:52:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class CashRegister
-  attr_accessor :total, :discount, :title, :price
+  attr_accessor :total, :discount, :title, :price, :cost
 
   def initialize(discount = 0)
     @array = []
