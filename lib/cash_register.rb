5c222b177c25fc4b7caaa00f192473e25c304d9e
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-17 14:10:49 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,17 +4,12 @@ class CashRegister
   attr_accessor :total, :discount, :title, :price, :cost
 
   def initialize(discount = 0)
-    @array = []
     @total = total
     @total = 0
     @discount = discount
-    @array << @total
+    
   end
 
-  def total 
-    array << @total.count
-  end 
-  
 
   def add_item(title, price, cost = 1)
    title = cost * price
